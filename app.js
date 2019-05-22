4781f1e6a4ced98b72d515559a67d9dbf25c5e14
Michael KS <kingstockton@gmail.com>
2019-05-22 15:15:16 -0600

    add get and placeholder from post

---
+++ app.js
@@ -0,0 +1,43 @@
+
+const express = require('express')
+const app = express()
+
+import cors from 'cors';
+app.use(cors());
+const ids = require('shortid')
+
+// ids.generate();  // "aeaf15"
+
+app.use(express.json())
+
+app.locals.notes = [{
+  title: 'test',
+  id: ids.generate(),
+  list: 
+  [{title: 'testtitle', id: ids.generate()}]
+}]
+
+
+app.get('/api/notes', (request, response) => {
+  const notes = app.locals.notes
+  return response.status(200).json(notes)
+})  
+
+app.get('/api/notes/:id', (request, response) => {
+const { id } = request.params;
+
+const note = app.locals.notes.forEach(note => {
+    if(note.list.includes(id)) return response.status(200).json(note)})
+    return response.status(404).json('Not Found')
+})
+
+//wait to pass this when current card is generated
+app.post('/api/notes/:id', (request, response) => {
+  const { list, title } = request.body
+  const id = ids.generate()
+})
+
+
+
+
+export default app
---
+++ server s.js
@@ -0,0 +1,9 @@
+import app from './app';
+app.set('port', process.env.PORT || 3000)
+
+
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.notes} is running on http://localhost:${app.get('port')}.`);
+});
+
