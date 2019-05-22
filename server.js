9cb3f35e7262bd505e1fdff75d2926ece3a219b1
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-05-22 14:07:15 -0600

    Delete method

--- server.js
+++ server.js
@@ -16,8 +16,13 @@ app.get('/api/notes', (request, response) => {
   return response.status(200).json(notes)
 })  
 
-
-
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.notes} is running on http://localhost:${app.get('port')}.`);
 });
+
+app.delete('/api/v1/notes/:id', (request, response) => {
+  const noteIndex = app.locals.notes.findIndex(note => note.id == request.params.id);
+  if (noteIndex === -1) return response.status(404).json('Note not found');
+  app.locals.notes.splice(noteIndex, 1);
+  return sendMessage(response, 200, 'Note was successfully deleted');
+})
