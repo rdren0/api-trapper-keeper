9edb44b6f4ad2f13290d030a75a2d3429228c0d0
Rachael Drennan <rdrennan0@gmail.com>
2019-05-22 14:02:55 -0600

    add PUT

--- server.js
+++ server.js
@@ -16,6 +16,27 @@ app.get('/api/notes', (request, response) => {
   return response.status(200).json(notes)
 })  
 
+app.put('/api/notes', (req, res) => {
+  const { title, items } = req.body;
+  let { id } = req.params;
+  id = parseInt(id);
+  let noteWasFound = false;
+  const newNote = app.locals.notes.map(note => {
+    if (note.id === id) {
+      noteWasFound = true;
+      return { id, title, items };
+    } else {
+      return note;
+    }
+  });
+
+  if (!title || !items) return res.status(422).json('Please provide a title and at least one item');
+  if (!noteWasFound) return res.status(404).json('Note not found');
+
+  app.locals.notes = newNotes;
+  return res.sendStatus(204);
+});
+
 
 
 app.listen(app.get('port'), () => {
