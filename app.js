7c14911077b77240dabd64a1f10f792599c3b7ba
Michael KS <kingstockton@gmail.com>
2019-05-23 12:07:18 -0600

    update get:id for server endpoint with correct response status and info
    Co-Authored-by: Matthew Kaplan <kaplan.matthew.p@gmail.com>
    Co-authored-by: Rachael Drennan <rdrennan0@gmail.com>

--- app.js
+++ app.js
@@ -15,19 +15,17 @@ app.get("/api/notes", (request, response) => {
 });
 
 app.get("/api/notes/:id", (request, response) => {
-  const id = request.params.id;
+  const { id } = request.params
+  const notes = app.locals.notes;
 
-  app.locals.notes.find(note => {
-    if (note.id === id) {
-      return response.status(200).json(note);
-    } else {
-      return response.status(404).json("Not Found");
-    }
-  });
+  const member = notes.find(member => member.id == id)
+  if(!member) return response.status(404).json({Error: `No note found with ${id}`})
+  return response.status(200).json(member)
 });
 
 //wait to pass this when current card is generated
 app.post("/api/notes/", (request, response) => {
+
   const { notes } = app.locals;
   const { title, list } = request.body;
 
@@ -45,9 +43,9 @@ app.post("/api/notes/", (request, response) => {
   return response.status(201).json(newlist);
 });
 
-app.put("/api/notes", (req, res) => {
-  const { title, items } = req.body;
-  let { id } = req.params;
+app.put("/api/notes", (reequest, response) => {
+  const { title, items } = reequest.body;
+  let { id } = reequest.params;
   id = parseInt(id);
   let noteWasFound = false;
   const newNote = app.locals.notes.map(note => {
@@ -60,11 +58,11 @@ app.put("/api/notes", (req, res) => {
   });
 
   if (!title || !items)
-    return res.status(422).json("Please provide a title and at least one item");
+    return response.status(422).json("Please provide a title and at least one item");
   if (!noteWasFound) return res.status(404).json("Note not found");
 
   app.locals.notes = newNotes;
-  return res.sendStatus(204);
+  return response.sendStatus(204);
 });
 
 const sendMessage = (response, code, message) => {
@@ -72,9 +70,11 @@ const sendMessage = (response, code, message) => {
 };
 
 app.delete("/api/v1/notes/:id", (request, response) => {
+  
   const noteIndex = app.locals.notes.findIndex(
     note => note.id == request.params.id
   );
+  console.log('noteINde', noteIndex)
   if (noteIndex === -1) return response.status(404).json("Note not found");
   app.locals.notes.splice(noteIndex, 1);
   return sendMessage(response, 200, "Note was successfully deleted");
