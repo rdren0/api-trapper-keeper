101fd5f6f22a25bd981a02243710b166f1606301
Rachael Drennan <rdrennan0@gmail.com>
2019-05-24 09:22:49 -0600

    prettier all tests

--- app.spec.js
+++ app.spec.js
@@ -116,38 +116,41 @@ describe("App", () => {
     });
 
     it("should return a status 204 when successful", async () => {
-      const response = await request(app).put("/api/v1/notes/2").send(newNote);
+      const response = await request(app)
+        .put("/api/v1/notes/2")
+        .send(newNote);
       expect(response.status).toBe(204);
-
     });
 
     it("should return a status 404 and an Error message if note does not exist", async () => {
-      const response = await request(app).put("/api/v1/notes/5").send(newNote);
+      const response = await request(app)
+        .put("/api/v1/notes/5")
+        .send(newNote);
       expect(response.status).toBe(404);
-      expect(response.body).toEqual({Error: "No note found with 5 "})
+      expect(response.body).toEqual({ Error: "No note found with 5 " });
     });
 
     it("should return a status 422 if the correct params are not sent", async () => {
-      const response = await request(app).put("/api/v1/notes/2").send(incompleteNote);
+      const response = await request(app)
+        .put("/api/v1/notes/2")
+        .send(incompleteNote);
       expect(response.status).toBe(422);
     });
   });
 
   describe("DELETE /api/v1/notes", () => {
     it("should delete the correct note", async () => {
-      const response = await request(app).delete("/api/v1/notes/2")
-      expect(notes.length).toBe(1)
-
+      const response = await request(app).delete("/api/v1/notes/2");
+      expect(notes.length).toBe(1);
     });
     it("return a status of 200 when complete", async () => {
-      const response = await request(app).delete("/api/v1/notes/2")
-      expect(response.status).toBe(200)
-
+      const response = await request(app).delete("/api/v1/notes/2");
+      expect(response.status).toBe(200);
     });
 
     it("should return a status of 404 if note doesnt exist", async () => {
-      const response = await request(app).delete("/api/v1/notes/99")
-      expect(response.status).toBe(404)
+      const response = await request(app).delete("/api/v1/notes/99");
+      expect(response.status).toBe(404);
     });
   });
 });
