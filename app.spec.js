5fd794dad3f85ccffb1e207cd4c4e882fd3bd95f
Rachael Drennan <rdrennan0@gmail.com>
2019-05-24 09:22:09 -0600

    complete backend testing

--- app.spec.js
+++ app.spec.js
@@ -1,5 +1,4 @@
 const request = require("supertest");
-// import '@babel/polyfill';
 const app = require("./app");
 
 describe("App", () => {
@@ -101,31 +100,54 @@ describe("App", () => {
   });
   describe("PUT /api/v1/notes", () => {
     let newNote;
-    let updatedNote;
+    let incompleteNote;
     beforeEach(() => {
       newNote = {
         title: "Cleaning",
         list: [{ id: 1, text: "testing" }, { id: 3, text: "testing again" }]
       };
-      updatedNote = { title: "broken" };
+      incompleteNote = { title: "broken" };
     });
 
     it("should update an existing note", async () => {
       const response = await request(app)
         .put("/api/v1/2")
-        .send(updatedNote);
+        .send(newNote);
     });
-    it("should return a status 204 when successful", async () => {});
 
-    it("should return a status 404 if note does not exist", async () => {});
+    it("should return a status 204 when successful", async () => {
+      const response = await request(app).put("/api/v1/notes/2").send(newNote);
+      expect(response.status).toBe(204);
 
-    it("should return a status 422 if the correct params are not set", async () => {});
+    });
+
+    it("should return a status 404 and an Error message if note does not exist", async () => {
+      const response = await request(app).put("/api/v1/notes/5").send(newNote);
+      expect(response.status).toBe(404);
+      expect(response.body).toEqual({Error: "No note found with 5 "})
+    });
+
+    it("should return a status 422 if the correct params are not sent", async () => {
+      const response = await request(app).put("/api/v1/notes/2").send(incompleteNote);
+      expect(response.status).toBe(422);
+    });
   });
 
   describe("DELETE /api/v1/notes", () => {
-    it("should delete the correct note", async () => {});
-    it("return a status of 204 when complete", async () => {});
+    it("should delete the correct note", async () => {
+      const response = await request(app).delete("/api/v1/notes/2")
+      expect(notes.length).toBe(1)
 
-    it("should return a status of 404 if note doesnt exist", async () => {});
+    });
+    it("return a status of 200 when complete", async () => {
+      const response = await request(app).delete("/api/v1/notes/2")
+      expect(response.status).toBe(200)
+
+    });
+
+    it("should return a status of 404 if note doesnt exist", async () => {
+      const response = await request(app).delete("/api/v1/notes/99")
+      expect(response.status).toBe(404)
+    });
   });
 });
